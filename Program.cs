bcf1132850e8a09b73774b17205fb374ebb3823a
Gerber Rogelio Díaz Santay <rogerdisan18@gmail.com>
2026-02-22 23:40:44 -0600

    Menu y entrada implementados

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿Console.WriteLine("=== Evaluacion de Prestamo ===");
+Console.WriteLine("Tipo de solicitante:");
+Console.WriteLine("1. Empleado fijo");
+Console.WriteLine("2. Temporal");
+Console.WriteLine("3. Independiente");
+Console.WriteLine("4. Estudiante");
+Console.Write("Seleccione: ");
+
+int tipo = int.Parse(Console.ReadLine());
+
+Console.Write("Ingreso mensual: ");
+double ingreso = double.Parse(Console.ReadLine());
+
+Console.Write("Antiguedad laboral (meses): ");
+int antiguedad = int.Parse(Console.ReadLine());
+
+Console.Write("Monto solicitado: ");
+double monto = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Historial crediticio:");
+Console.WriteLine("1. Excelente");
+Console.WriteLine("2. Bueno");
+Console.WriteLine("3. Regular");
+Console.WriteLine("4. Malo");
+Console.Write("Seleccione: ");
+int historial = int.Parse(Console.ReadLine());
+
+Console.Write("Tiene fiador (S/N): ");
+char fiador = char.Parse(Console.ReadLine().ToUpper());
